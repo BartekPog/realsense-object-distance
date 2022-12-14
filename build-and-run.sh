b3d6f83cd3af4076eddcc791025db70898b6ee13
PiotrHasiec <piotrhasiec@op.pl>
2022-12-14 13:24:45 -0600

    Update entrypoint and run script

--- build-and-run.sh
+++ build-and-run.sh
@@ -1,2 +1,2 @@
 docker build -f Dockerfile --network=host -t realsense-build-depth -t realsense-build-depth:latest . 
-sudo docker run --privileged -it -p 8082:80 --rm realsense-build-depth
+sudo docker run --privileged -it --rm realsense-build-depth
